de980792f6f842cb9b831bbd934a1f4112297d30
John Benninghoff <jbenninghoff@mac.com>
2026-02-23 16:14:28 -0600

    Add basic relay testing script

---
+++ tests/recipients.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cat <<'EOF'
+# Test that Postfix accepts mail for defined virtual users and aliases only
+# requires email: nasmail@nasmail.test, aliases: postmaster@nasmail.test, abuse@nasmail.test
+# other recipients should be rejected with error 550 or 454
+
+HELO localhost
+MAIL From: nobody@example.com
+
+# these should be accepted
+RCPT To: nasmail@nasmail.test
+RCPT To: postmaster@nasmail.test
+RCPT To: abuse@nasmail.test
+
+# these should be rejected
+RCPT To: nobody@nasmail.test
+RCPT To: nasmail@mail.nasmail.test
+RCPT To: nobody@example.com
+
+DATA
+Subject: Test email
+This is a test email.
+.
+QUIT
+
+EOF
+telnet mail.nasmail.test 25
