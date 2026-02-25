719cfe30bf358c87bf6cd10ac6e3040e883b2b36
John Benninghoff <jbenninghoff@mac.com>
2026-02-25 10:55:25 -0600

    fix: improved postfix snapshot test

--- tests/postconf.sh
+++ tests/postconf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # check for changes to postconf default configuration
-docker run -it --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main | colordiff -u snapshot-postconf.txt -
+docker run --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main | colordiff -u snapshot-postconf.txt -
 
-echo 'snapshot using: docker run -it --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main >snapshot-postconf.txt'
-echo 'docker run -it --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main >snapshot-postconf.txt' | pbcopy
+echo 'snapshot using: docker run --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main >snapshot-postconf.txt'
+echo 'docker run --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main >snapshot-postconf.txt' | pbcopy
