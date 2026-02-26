2463c25104738cf319ff54cb8b6692e263f88813
John Benninghoff <jbenninghoff@mac.com>
2026-02-26 07:55:24 -0600

    test: use locally built image for testing

--- tests/postconf.sh
+++ tests/postconf.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # check for changes to postconf default configuration
-docker run --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main | colordiff -u snapshot-postconf.txt -
+# requires:  docker build -t nasmail .
+docker run --rm --entrypoint postconf nasmail | colordiff -u snapshot-postconf.txt -
 
-echo 'snapshot using: docker run --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main >snapshot-postconf.txt'
-echo 'docker run --rm --entrypoint postconf ghcr.io/jabenninghoff/nasmail:main >snapshot-postconf.txt' | pbcopy
+echo 'snapshot using: docker run --rm --entrypoint postconf nasmail >snapshot-postconf.txt'
+echo 'docker run --rm --entrypoint postconf nasmail >snapshot-postconf.txt' | pbcopy
