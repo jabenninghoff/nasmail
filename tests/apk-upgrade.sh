4fb39b73190aa36174b0d96cc6d0ae27ebd196bb
John Benninghoff <jbenninghoff@mac.com>
2026-03-13 15:24:24 -0500

    test: clarify and improve apk-upgrade

--- tests/apk-upgrade.sh
+++ tests/apk-upgrade.sh
@@ -2,9 +2,13 @@
 # check for apk package updates
 # requires: docker-build.sh
 
-echo "checking ghcr.io/jabenninghoff/nasmail:latest"
+echo "apk-upgrade: pulling latest image from ghcr.io/jabenninghoff/nasmail:latest"
+docker pull ghcr.io/jabenninghoff/nasmail:latest
+
+echo ""
+echo "apk-upgrade: running apk upgrade --no-cache in ghcr.io/jabenninghoff/nasmail:latest"
 docker run --rm --entrypoint apk ghcr.io/jabenninghoff/nasmail:latest upgrade --no-cache
 
 echo ""
-echo "checking nasmail:dev"
+echo "apk-upgrade: running apk upgrade --no-cache in nasmail:dev"
 docker run --rm --entrypoint apk nasmail:dev upgrade --no-cache
