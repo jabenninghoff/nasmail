713d64c6c411dc6ab8939a498792cb281db6f00f
John Benninghoff <jbenninghoff@mac.com>
2026-03-31 12:00:08 -0500

    test: support cross-platform apk testing

--- tests/apk-list.sh
+++ tests/apk-list.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # check for changes to installed packages
 # requires: docker-build.sh
-docker run --rm --entrypoint apk nasmail:dev list | colordiff -u snapshot-apk.txt -
+docker run --rm --entrypoint apk nasmail:dev list --no-cache --installed --manifest | colordiff -u snapshot-apk.txt -
 
-echo 'snapshot using: docker run --rm --entrypoint apk nasmail:dev list >snapshot-apk.txt'
-echo 'docker run --rm --entrypoint apk nasmail:dev list >snapshot-apk.txt' | pbcopy
+echo 'snapshot using: docker run --rm --entrypoint apk nasmail:dev list --no-cache --installed --manifest >snapshot-apk.txt'
+echo 'docker run --rm --entrypoint apk nasmail:dev list --no-cache --installed --manifest >snapshot-apk.txt' | pbcopy
