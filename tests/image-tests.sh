5692876777b7e78c7019eb8615ed3d09d6ae9db7
John Benninghoff <jbenninghoff@mac.com>
2026-07-13 19:00:17 -0500

    test: TODO: consider automated test suite

--- tests/image-tests.sh
+++ tests/image-tests.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # run all image tests
 # requires: docker-build.sh
+
+# TODO: consider automated test suite similar to https://github.com/ServerContainers/minimail
+
 ./apk-list.sh && \
 ./postconf-defaults.sh && \
 ./postconf.sh && \
